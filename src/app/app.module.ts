8a7f0ed2156ec93222b5880ce2495e749f33730b
Abhishek Joshi <abhishek.joshi20@gmail.com>
2018-07-31 22:48:08 -0400

    updated pkg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { MobxAngularModule } from 'mobx-angular';  
+import { BirthdayStore } from '../stores/birthday.store';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -34,6 +37,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    BirthdayStore,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
